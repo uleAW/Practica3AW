7c72a06f185c28a5867cf36b18c1abe4da32c037
rsevig00 <rsevig00@estudiantes.unileon.es>
2021-05-16 21:38:31 +0200

    Solucionado fallo servidor y cargado express

--- Practica Grupal AW/server.js
+++ Practica Grupal AW/server.js
@@ -4,14 +4,11 @@ const port = 5050
 const host = '127.0.0.1'
 const path = require('path')
 
-app.use('/html', express.static('html'));
-app.use('/css', express.static('css'));
-app.use('/js', express.static('js'));
-app.use('/img', express.static('img'));
-//app.use('/principal', express.static('principal'));
+app.use('/src', express.static('src'));
+app.use('/media', express.static('media'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'src/index.html'))
 })
 
 app.listen(port, () => {
