50440218e0ce9d2ba2d583c25647959bd51ec5af
dnarcr00 <47595588+dnarcr00@users.noreply.github.com>
2021-06-04 20:55:28 +0200

    Actualizacion a js con node

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const port = 5050
 const host = '127.0.0.1'
 const path = require('path')
 
-app.use('/src', express.static('src'));
-app.use('/media', express.static('media'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/index.html'))
-})
+//Permite servir paginas estaticas con cualquier extension
+app.use(express.static(__dirname + '/'));
+
+//Listener post
+app.post('/', function (req, res) {
+    console.log("POST");
+});
+
+//Listener get
+app.get('/',function (req, res) {
+    console.log("GET");
+});
 
 app.listen(port, () => {
     console.log(`Listening at http://${host}:${port}`)
