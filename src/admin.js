aa72950bdb117a3888ea1b0e416fef6882d792ee
rsevig00 <rsevig00@estudiantes.unileon.es>
2021-06-13 20:58:57 +0200

    Actualizado admin

--- src/admin.js
+++ src/admin.js
@@ -31,8 +31,60 @@ window.onblur=window.onmousemove=function() {
     contadorInactividad();
 }
 
-function cerrarSesion(){
-    localStorage.removeItem("user");
-    localStorage.removeItem("pass");
-    window.open("/index.html","_self");  
+function aniadirColeccion() {
+    var nombre = document.getElementById("nombreAniadirColeccion").value
+    var imagen = document.getElementById("imgAniadirAlbum").files[0]
+    var precioAlbum = document.getElementById("precioAniadirAlbum").value
+    var reader = new FileReader()
+
+    reader.onload = function(event) {
+
+        var data = event.target.result.replace("data:" + imagen.type+";base64,", '');
+        fetch("/aniadirColeccion", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"nombre": nombre, "imagen": data, "precio": precioAlbum})
+        }).then(response => {
+            if (response.status == 200) {
+                // Mostrar pagina de colecciones o volver a la pestaña de atras
+                console.log("Colección añadida")
+            } else {
+                // Mostrar error de conexion
+                console.log("Error al conectar")
+            }
+        })
+    }
+    reader.readAsDataURL(imagen);
+}
+
+function aniadirCromo() {
+
+    var nombre = document.getElementById("nombreAniadirCromo").value
+    var imagen = document.getElementById("imgAniadirCromo").files[0]
+    var precioCromo = document.getElementById("precioAniadirCromo").value
+    var coleccion = document.getElementById("coleccionAniadirCromo").value
+    var reader = new FileReader()
+
+    reader.onload = function(event) {
+
+        var data = event.target.result.replace("data:" + imagen.type+";base64,", '');
+        fetch("/aniadirCromo", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"nombre": nombre, "imagen": data, "precio": precioCromo, "coleccion": coleccion})
+        }).then(response => {
+            if (response.status == 200) {
+                // Mostrar pagina de colecciones o volver a la pestaña de atras
+                console.log("Cromo añadido")
+            } else {
+                // Mostrar error de conexion
+                console.log("Error al conectar")
+            }
+        })
+    }
+    reader.readAsDataURL(imagen);
 }
