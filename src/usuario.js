83f738f619fea7ce88615a64f0942295200b134f
rsevig00 <rsevig00@estudiantes.unileon.es>
2021-06-15 23:09:42 +0200

    Cambio js

--- src/usuario.js
+++ src/usuario.js
@@ -191,7 +191,9 @@ async function cargarAlbumes() {
         newDiv.onclick = (function (i) {
             return function () {
                 localStorage.setItem("cargarCromosUsuario", nombresAlbumes[i])
-                window.open("./cromosUsuario.html", "_self");
+                // window.open("./cromosUsuario.html", "_self");
+                document.getElementById("menu").style.display = "none"
+                cargarCromos();
             }
         })(i);
         document.getElementById('menu').appendChild(newDiv);
@@ -243,6 +245,60 @@ async function cargarAlbumes() {
         newDiv.innerHTML = numCromos[i];
         document.getElementById('texto' + i).appendChild(newDiv);
     }
+}
+
+async function cargarCromos() {
+    var imgCromo;
+    var nombreCromo;
+    imgCromo = await cargarImagenesCromos();
+    imgCromo = imgCromo.split(',')
+    nombreCromo = await cargarNombresCromo();
+    nombreCromo = nombreCromo.split(',')
+    for (var i = 0; i < imgCromo.length - 1; i++) {
+        var newDiv = document.createElement('div');
+        newDiv.id = 'cromo' + i;
+        newDiv.className = 'cromos';
+        document.getElementById('menuCromos').appendChild(newDiv);
+        //document.getElementById('pasatiempo'+i).onclick=function() { table(IDColecciones[i]); };;
+
+        newDiv = document.createElement('img');
+        newDiv.id = 'img' + i;
+        newDiv.className = 'image';
+        newDiv.src = "data:image/png;base64," + imgCromo[i];
+        document.getElementById('cromo' + i).appendChild(newDiv);
+
+        newDiv = document.createElement('text');
+        newDiv.id = 'text' + i;
+        newDiv.className = 'text';
+        newDiv.innerHTML = nombreCromo[i];
+        document.getElementById('cromo' + i).appendChild(newDiv);
+
+    }
+    document.getElementById("menuCromos").style.display = "block";
+}
+
+function cargarImagenesCromos() {
+    return fetch("/imagenCromosUsuario", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"nombreAlbum": localStorage.getItem("cargarCromosUsuario"), "nombreUsuario": localStorage.getItem("user")})
+    }).then(response => response.text().then(function (text) {
+        return text;
+    }));
+}
+
+function cargarNombresCromo() {
+    return fetch("/nombreCromosUsuario", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"nombreAlbum": localStorage.getItem("cargarCromosUsuario"), "nombreUsuario": localStorage.getItem("user")})
+    }).then(response => response.text().then(function (text) {
+        return text;
+    }));
 }
 
 function mostrarAlbumes() {
